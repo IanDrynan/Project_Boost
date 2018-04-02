8acee7892c5aa00ee81ba6c46e0c0a50d012f83f
Ian Drynan <drynanian@gmail.com>
2018-04-01 22:08:02 -0600

    Everything working at this commit
    Before I mess with everything for fun

---
+++ Assets/Scripts/Oscillate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillate : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector = new Vector3(10, 10, 10);
+    [SerializeField] float period = 10f;
+
+    [Range(0, 1)] [SerializeField] float movementFactor;
+
+    Vector3 startingPos;
+
+	// Use this for initialization
+	void Start () {
+        startingPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (period <= Mathf.Epsilon) { return; }
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave;
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+	}
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody ship_rigidbody;
+    AudioSource ship_audio;
+    Vector3 thrust;
+    Vector3 direction;
+    bool Alive = true;
+    int nextSceneIndex;
+    [SerializeField] float thrust_Speed = 1;
+    [SerializeField] float max_Thrust = 3;
+    [SerializeField] float turn_Speed = 1;
+    [SerializeField] AudioClip win;
+    [SerializeField] AudioClip lose;
+    [SerializeField] AudioClip engine;
+    [SerializeField] ParticleSystem engineParticles;
+    [SerializeField] ParticleSystem winParticles;
+    [SerializeField] ParticleSystem loseParticles;
+    
+
+    // Use this for initialization
+    void Start () {
+        ship_rigidbody = GetComponent<Rigidbody>();
+        ship_audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Alive)
+        {
+            HandleInput();
+        }
+        
+    }
+
+    private void HandleInput()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
+        ship_rigidbody.velocity = Vector3.ClampMagnitude(ship_rigidbody.velocity, max_Thrust);
+        
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            ApplyThrust();
+            engineParticles.Play();
+        }
+        else
+        {
+            ship_audio.Stop();
+            engineParticles.Stop();
+        }
+    }
+
+    private void ApplyThrust()
+    {
+        ship_rigidbody.AddRelativeForce(Vector3.up * thrust_Speed);
+
+        if (!ship_audio.isPlaying)
+        {
+            ship_audio.PlayOneShot(engine);
+
+        }
+    }
+
+    private void Rotate()
+    {
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            ship_rigidbody.angularVelocity = Vector3.zero;
+            direction = new Vector3(0, 0, Input.GetAxis("Horizontal") * -1);
+            transform.Rotate(direction * turn_Speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!Alive) { return; }
+        switch (collision.gameObject.tag)
+        {
+            case "Friend":
+                break;
+            case "Goal":
+                StartSuccessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }        
+    }
+
+    private void StartDeathSequence()
+    {
+        Alive = false;
+        ship_audio.Stop();
+        ship_audio.PlayOneShot(lose);
+        loseParticles.Play();
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex, 1f));
+    }
+
+    private void StartSuccessSequence()
+    {
+        Alive = false;
+        ship_audio.Stop();
+        ship_audio.PlayOneShot(win);
+        winParticles.Play();
+        nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (SceneManager.sceneCountInBuildSettings > nextSceneIndex)
+        {
+            StartCoroutine(LoadLevel(nextSceneIndex, 1f));
+        }
+        
+    }
+
+    IEnumerator LoadLevel(int num, float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        SceneManager.LoadScene(num);
+    }
+}
